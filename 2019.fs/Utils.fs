de20a7b4f1bc876dde7fe1e31c08c481268f2618
Uluç Şengil <u.sengil@maul.utoronto.ca>
2019-12-01 22:54:51 -0500

    add  util function to get whole input as string

--- 2019.fs/Utils.fs
+++ 2019.fs/Utils.fs
@@ -4,12 +4,18 @@ open FSharp.Data
 
 module Utils =
     let splitLines (text: string) = text.Split("\n")
+    let trimString (text: string) = text.Trim()
 
-    let getInputLines day =
+    let getInput day =
         Http.RequestString
             (sprintf "https://adventofcode.com/2019/day/%d/input" day,
              cookies =
                  [ "session",
                    "***REMOVED***" ])
+        |> trimString
+
+
+    let getInputLines day =
+        getInput day
         |> splitLines
         |> Array.filter (fun a -> a.Length > 0)
