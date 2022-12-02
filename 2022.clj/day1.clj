cef49ef058ac2103331c973cc1891590095d1d42
Uluç Şengil <u.sengil@mail.utoronto.ca>
2022-12-02 08:38:13 +0300

    day 2 done

--- 2022.clj/day1.clj
+++ 2022.clj/day1.clj
@@ -1,13 +1,13 @@
 (ns day1
   (:require [clojure.string :as str]))
 
-(def input (slurp "./inputs/1.txt"))
+(def input (slurp "2022.clj/inputs/1.txt"))
 
 (def cals (->> (str/split input #"\n\n")
                (map #(str/split % #"\n"))
                (map #(map parse-long %))
                (map #(reduce + %))))
 
-(println (reduce max cals))
-(println (reduce + (take 3 (sort cals))))
+(reduce max cals)
+(reduce + (take 3 (sort cals)))
 
---
+++ 2022.clj/day2.clj
@@ -0,0 +1,34 @@
+(ns day2
+  (:require [clojure.string :as str]))
+
+(def input (-> (slurp "2022.clj/inputs/2.txt") (str/split #"\n")))
+
+(defn eval-game [ips]
+  (case ips
+    "A X" [1 1] ; rock
+    "B X" [0 1] ; paper
+    "C X" [2 1] ; scissors
+    "A Y" [2 2]
+    "B Y" [1 2]
+    "C Y" [0 2]
+    "A Z" [0 3]
+    "B Z" [2 3]
+    "C Z" [1 3]))
+
+(defn eval-game2 [ips]
+  (case ips
+    "A X" [0 3] ; lose
+    "A Y" [1 1] ; draw
+    "A Z" [2 2] ; win
+    "B X" [0 1]
+    "B Y" [1 2]
+    "B Z" [2 3]
+    "C X" [0 2]
+    "C Y" [1 3]
+    "C Z" [2 1]))
+
+(->>  [eval-game eval-game2]
+      (map (fn [eg] (->> input
+                         (map eg)
+                         (map #(+ (* 3 (first %)) (second %)))
+                         (reduce +)))))
