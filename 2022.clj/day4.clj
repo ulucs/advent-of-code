3d5aaa2ccbf409f4c714e082c2ad9ec5d8533c2b
Uluç Şengil <u.sengil@mail.utoronto.ca>
2022-12-04 08:40:21 +0300

    get rid of some [[]]'s

--- 2022.clj/day4.clj
+++ 2022.clj/day4.clj
@@ -1,26 +1,23 @@
 (load-file "2022.clj/utils.clj")
-
 (ns day4
   (:require [utils :as u])
   (:require [clojure.string :as str]))
 
 (u/dl-input 4)
-
 (def input (u/read-input-lines 4))
 
 (->> input
      (map #(str/split % #","))
-     (map (fn [x] (map #(str/split % #"-") x)))
-     (map (fn [x] (map (fn [y] (map parse-double y)) x)))
-     (map (fn [x] (sort-by (fn [[a b]] (- a b)) x)))
-     (filter (fn [[[a1 b1] [a2 b2]]] (or (<= a1 a2) (>= b1 b2))))
+     (map (partial map #(str/split % #"-")))
+     (map (partial map (partial map parse-long)))
+     (map (partial sort-by (partial apply -)))
+     (filter (fn [[[a1 b1] [a2 b2]]] (and (<= a1 a2) (>= b1 b2))))
      (count))
 
 (->> input
      (map #(str/split % #","))
-     (map (fn [x] (map #(str/split % #"-") x)))
-     (map (fn [x] (map (fn [y] (map parse-double y)) x)))
-     (map (fn [x] (sort-by (fn [[a]] a) x)))
+     (map (partial map #(str/split % #"-")))
+     (map (partial map (partial map parse-long)))
+     (map (partial sort-by first))
      (filter (fn [[[_ b1] [a2 _]]] (>= b1 a2)))
      (count))
-
